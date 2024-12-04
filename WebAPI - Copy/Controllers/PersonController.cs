e1ad1e0131800a6c166f8903883b9c3a7018f1e8
Washim Akram <90306771+AlienWashim@users.noreply.github.com>
2024-12-04 21:29:28 +0600

    Source code uploaded

---
+++ WebAPI - Copy/Controllers/PersonController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly DatabaseServices _databaseServices;
+
+        public PersonController(DatabaseServices databaseServices)
+        {
+            _databaseServices = databaseServices;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> GetPersons()
+        {
+            var persons = _databaseServices.GetPersons();
+            return persons;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Person> GetPersonById(int id)
+        {
+            var person = _databaseServices.GetPersonById(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public ActionResult PostPerson([FromBody] Person person)
+        {
+            _databaseServices.AddPerson(person);
+
+            return CreatedAtAction(nameof(GetPersons), new { Id = person.Id }, person);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult PutPerson(int id, [FromBody] Person person)
+        {
+            var oldPerson = _databaseServices.GetPersonById(id);
+            if (oldPerson == null)
+            {
+                return NotFound();
+            }
+
+            person.Id = id;
+
+            _databaseServices.UpdatePerson(person);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeletePerson(int id)
+        {
+            Person oldPerson = _databaseServices.GetPersonById(id);
+            if(oldPerson == null)
+            {
+                return NotFound();
+            }
+
+            _databaseServices.DeletePerson(id);
+            return NoContent();
+        }
+    }
+}
---
+++ WebAPI - Copy/Services/DatabaseServices.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public class DatabaseServices
+    {
+        private readonly string _connectionString;
+        public DatabaseServices(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+        public List<Person> GetPersons()
+        {
+            List<Person> persons = new List<Person>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand("GetAllPersons", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            persons.Add(new Person
+                                {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                Salary = reader.GetDecimal(2),
+                            });
+                        }
+                    }
+                }
+            }
+
+            return persons;
+        }
+
+        public Person GetPersonById(int id)
+        {
+            Person person = null;
+
+            using (var  sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlCommand command = new SqlCommand("GetPersonById", sqlConnection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", id);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            person = new Person
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                Salary = reader.GetDecimal(2)
+                            };
+                        }
+                    }
+                }
+            }
+            return person;
+        }
+
+        public void AddPerson(Person person)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand("AddPerson", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", person.Id);
+                    command.Parameters.AddWithValue("@Name", person.Name);
+                    command.Parameters.AddWithValue("@Salary", person.Salary);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand("UpdatePerson", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", person.Id);
+                    command.Parameters.AddWithValue("@Name", person.Name);
+                    command.Parameters.AddWithValue("@Salary", person.Salary);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeletePerson(int id)
+        {
+            using (var  sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                using (var command = new SqlCommand("DeletePerson", sqlConnection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
